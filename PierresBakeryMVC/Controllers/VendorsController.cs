3becfbcd894f4a12fba1cb9050646622ad7c56aa
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-24 12:48:45 -0700

    add /vendors view and html

--- PierresBakeryMVC/Controllers/VendorsController.cs
+++ PierresBakeryMVC/Controllers/VendorsController.cs
@@ -7,6 +7,11 @@ namespace PierresBakeryMVC.Controllers
 {
   public class VendorsController : Controller
   {
-    
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
   }
 }
