0a3df5dc3d9ca015467cd4d679fa76fa067d8af3
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-24 13:01:08 -0700

    add New html and route in VendorsController

--- PierresBakeryMVC/Controllers/VendorsController.cs
+++ PierresBakeryMVC/Controllers/VendorsController.cs
@@ -13,5 +13,18 @@ namespace PierresBakeryMVC.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string desc)
+    {
+      Vendor newVendor = new Vendor(vendorName, desc);
+      return RedirectToAction("Index");
+    }
   }
 }
