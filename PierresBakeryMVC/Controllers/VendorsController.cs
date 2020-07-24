b0b4a2951b90f7b1786a43d53aaf9c7a94002daf
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-24 13:08:03 -0700

    add Show.cshtml code and HttpGet method in VendorsController

--- PierresBakeryMVC/Controllers/VendorsController.cs
+++ PierresBakeryMVC/Controllers/VendorsController.cs
@@ -26,5 +26,16 @@ namespace PierresBakeryMVC.Controllers
       Vendor newVendor = new Vendor(vendorName, desc);
       return RedirectToAction("Index");
     }
+
+    [HttpGet("vendors/{superCoolAmazingId}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
   }
 }
